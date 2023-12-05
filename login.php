73a367dcbb98c65ea9d4a2cc18fe83440eb5aa05
zahra <habiballahi>
2023-12-05 17:16:43 +0330

    login page is added

---
+++ login.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./CSS/styles.css">
+    <link rel="stylesheet" href="CSS/bootstrap.css">
+    <script src="CSS/main.js"></script>
+    <title>ورود</title>
+</head>
+<body>
+<article class="register_form">
+    <h3> ورود</h3>
+<form method="post" action="welcom.php">
+    <div class="input_group">
+        <input class="contact_input" type="text" placeholder="نام کاربری" name="user_name">
+        <input class="contact_input" type="password" placeholder="رمزعبور" name="pass">
+    </div>
+    <button class="btn btn_submit" type="submit">ورود</button>
+</form>
+</article>
+</body>
+</html>
--- welcom.php
+++ welcom.php
@@ -1,4 +1,49 @@
-<?php
-$username=$_GET['user'];
-        echo "welcom  ".$username;
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./CSS/styles.css">
+    <link rel="stylesheet" href="CSS/bootstrap.css">
+    <script src="CSS/main.js"></script>
+    <title>admin</title>
+</head>
+<body>
+<div class="course_title" id="courses_mode"><?php auth(); ?></div>
+<section class="teacher clearfix list">
+<article class="list-allcard">
+    <a href="manage_car.php?index=car">
+    <div class="list-card">
+        <img class="teacher-img clearfix" src="./images/car.png" alt="teacher-pic">
+        <h4 class="teacher-name">مدیریت ماشین </h4>
+    </div>
+    </a>
+    <a href="manage_car.php?index=customer">
+    <div class="list-card">
+        <img class="teacher-img" src="./images/user.png" alt="teacher-pic">
+        <h4 class="teacher-name">مدیریت مشتری</h4>
+    </div>
+    </a>
+</article>
+</section>
+</body>
+</html>
 
+
+<?php
+
+
+function auth()
+{
+
+    $username = $_POST['user_name'];
+    $password = $_POST['pass'];
+    $connect = mysqli_connect("localhost", "root", "", "car_gallery");
+    $sql = "select * from user where user_name='$username'";
+    $result = mysqli_query($connect, $sql);
+    $user = mysqli_fetch_array($result);
+
+    if ($password == $user['password'])
+        echo "welcom  " . $username;
+    else
+        header("location:login.php");
+}
